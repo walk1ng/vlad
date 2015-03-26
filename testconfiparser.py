bc0c53f913ec57930c8095370f77d3e07f32d5dd
walk1ng <v-lii@OSTC-Wei.fareast.corp.microsoft.com>
2015-03-26 11:58:04 +0800

    generate tests.properties

--- testconfiparser.py
+++ testconfiparser.py
@@ -6,32 +6,40 @@ except ImportError, e:
 
 testConfigXmlName = "tests.xml"
 
-
 def Parse(xmlname):
 	tree = ET.parse(xmlname)
+	alltests = []
 	testMatrix={}
 	for cycle in tree.iterfind('cycles/cycle'):
 		cyclename = cycle.attrib['name']
 		testlist = []
-		for test in cycle.iterfind('test/name'):
+		for test in cycle.iterfind('test'):
 			testlist.append(test.text)
 		testMatrix[cyclename]=testlist
-	tests= []
+	
 	for c in testMatrix:
-
-
-
+		for tname in testMatrix[c]:
+				thatcase = tree.find('tests/test[@name=' + '"' + tname +'"' + ']')
+				if thatcase != None:
+					testcmd = thatcase[0].text
+					testowner = thatcase[1].text
+				test = {
+					"enabled" : 'true',
+					"owner" : testowner,
+					"testgroup" : c,
+					"testcase" : tname
+				}
+				alltests.append(test)
+	return alltests
 
 
 def GenerateTestPropertiesFile():
-	configstring = '''
-					enableField=enabled
-					groupBy=testgroup
-					fieldSeparator=.
-					showFields=testcase
-					multiplicityField=multiplicity
-					'''
+	configstring = "enableField=enabled\ngroupBy=testgroup\nfieldSeparator=.\nshowFields=testcase\nmultiplicityField=multiplicity"
+	out = Parse(testConfigXmlName)
+	out = out.__str__().replace("'true'","true")
+	out = out + '\n' + configstring
+	with open('tests.properties','w') as ff:
+		ff.write(out)
 
-
-
+GenerateTestPropertiesFile()
 
