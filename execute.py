e1bb4cbc3edbe644e033d4e66dad7a93b6b176f8
walk1ng <v-lii@OSTC-Wei.fareast.corp.microsoft.com>
2015-03-27 14:59:31 +0800

    support run selected tests

---
+++ execute.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+import os
+import re
+import json
+try:
+	import xml.etree.cElementTree as ET
+except ImportError, e:
+	import xml.etree.ElementTree as ET
+
+tests = os.environ("alltests")
+testConfigXmlName = "tests.xml"
+
+# retrieve test case
+def RetrieveCase():
+	cases_need_run = []
+	cases = (re.sub('},{','}*{', tests[1:-1])).split('*')
+	for c in cases:
+		j = json.loads(c)
+		casename = j.get('testcase','')
+		tree = ET.parse(testConfigXmlName)
+		casecmd = (tree.find('tests/test[@name=' + '"' + casename +'"' + ']'))[0].text
+		case = {'casename' : casename, 'casecmd' : casecmd}
+		cases_need_run.append(case)
+	return cases_need_run
+
+
+def Run(case):
+	casename, casecmd = case['name'], case['cmd']
+	print
+	print "*" * 30
+	print "TEST START: %s" % casename
+	print "EXECUTE COMMAND: %s" % casecmd
+	rtn = os.system(casecmd)
+	result=""
+	if rtn != 0:
+		result = "FAILED"
+	else:
+		result = "PASS"
+	print "TEST %s: %s" % (result, casename)
+	print "*" * 30
+	print
+ 
+ def ExecuteTest():
+ 	cases_need_run = RetrieveCase()
+ 	print "Below tests will be run:"
+ 	for c in cases_need_run:
+ 		print c['casename']
+ 	print
+ 	for c in cases_need_run:
+ 		Run(c)
+
+ExecuteTest()
